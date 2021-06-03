1a815f088252c5cb6f125c763c142055d26c8555
m-abdalllah <m.abdallah@foodics.com>
2021-06-03 21:41:13 +0200

    add try catch while connecting to db incase of any error in connection

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -17,7 +17,11 @@ class Database
     public function connect()
     {
         if ($this->pdo == null) {
-            $this->pdo = new PDO("sqlite:". Config::PATH_TO_SQLITE_FILE);
+            try {
+                $this->pdo = new PDO("sqlite:". Config::PATH_TO_SQLITE_FILE);
+            } catch (\PDOException $e) {
+                die($e->getMessage());
+            }
         }
 
         return $this->pdo;
